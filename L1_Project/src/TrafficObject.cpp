928e8ef643507294377f8fbaaee3bca4320e8918
Owen LEE <joker1251@naver.com>
2020-08-01 12:21:24 +0900

    (8/1) L1 task is done (1,2,3)

---
+++ L1_Project/src/TrafficObject.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <chrono>
+#include <algorithm>
+#include "TrafficObject.h"
+
+// init static variable
+int TrafficObject::_idCnt = 0;
+
+void TrafficObject::setPosition(double x, double y)
+{
+    _posX = x;
+    _posY = y;
+}
+
+void TrafficObject::getPosition(double &x, double &y)
+{
+    x = _posX;
+    y = _posY;
+}
+
+TrafficObject::TrafficObject()
+{
+    _type = ObjectType::noObject;
+    _id = _idCnt++;
+}
+
+TrafficObject::~TrafficObject()
+{
+    // Task L1.1 : Set up a thread barrier that ensures that all the thread objects in the member vector _threads are joined.
+    // (7/30) FIRST DO: 
+    // using lambda expression, arguing "std::threads&" and make it barrier with join() member
+    std::for_each(_threads.begin(),_threads.end(),[](std::thread& t){
+        t.join();});
+}
